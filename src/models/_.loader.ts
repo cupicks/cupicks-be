a22b108074dd03f8c0282a08284630cb01e5031e
Unchaptered <workstation19961002@gmail.com>
2022-08-30 16:51:00 +0900

    Create joi validator 랑 호환되는 dto 타입 생성

--- src/models/_.loader.ts
+++ src/models/_.loader.ts
@@ -1,3 +1,24 @@
 import Env from "./env/env";
+import {
+    CustomException,
+    ConflictException,
+    NotFoundException,
+    UnkownTypeError,
+    UnkownError,
+    UnOverrideDtoError,
+} from "./exceptions/custom.exception";
 
-export { Env };
+import { IBaseDto } from "./dtos/i.base.dto";
+import { SignupUserDto } from "./dtos/signup.user.dto";
+
+export {
+    Env,
+    IBaseDto,
+    SignupUserDto,
+    CustomException, // 500
+    NotFoundException, // 404
+    ConflictException, // 409
+    UnkownTypeError, // 500
+    UnkownError, // 500
+    UnOverrideDtoError, // 500
+};
