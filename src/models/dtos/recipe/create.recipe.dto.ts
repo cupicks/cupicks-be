c05d586a49b3c03034c36f08c76a5f4e6cbb1579
axis <axisotherwise@gmail.com>
2022-08-31 00:39:11 +0900

    Update 레시피 작성 기능 구현

--- src/models/dtos/recipe/create.recipe.dto.ts
+++ src/models/dtos/recipe/create.recipe.dto.ts
@@ -31,6 +31,28 @@ export class CreateRecipeDto implements IBaseDto {
     }
 
     getJoiInstance(): ObjectSchema<CreateRecipeDto> {
-        return joi.object<CreateRecipeDto>({});
+        return joi.object<CreateRecipeDto>({
+            title: joi.string().trim().min(2).max(20).required(),
+            content: joi.string().trim().max(255).required(),
+            isIced: joi.boolean().required(),
+            cupSize: joi.number().equal(355, 473, 591).required(),
+            ingredientList: joi.array().items(
+                joi.object({
+                    ingredientName: joi.string().trim().min(1).max(20).required(),
+                    ingredientColor: joi
+                        .string()
+                        .trim()
+                        .min(7)
+                        .max(7)
+                        .required()
+                        .custom((value, helper) => {
+                            if (!value.startsWith("#")) {
+                                throw new Error("색상은 #으로 시작합니다.");
+                            }
+                        }),
+                    ingredientAmount: joi.number().max(1000).required(),
+                }),
+            ),
+        });
     }
 }
