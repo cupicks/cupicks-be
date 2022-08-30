2e208c81361b5522e87d2d529041e564d763b820
Unchaptered <workstation19961002@gmail.com>
2022-08-30 10:38:06 +0900

    Sample 예시용 Router 추가

---
+++ src/routes/routers/_.exporter.ts
@@ -0,0 +1,3 @@
+import authRouter from "./auth.router";
+
+export { authRouter };
---
+++ src/routes/routers/auth.router.ts
@@ -0,0 +1,5 @@
+import { Router } from "express";
+
+const authRouter: Router = Router();
+
+export default authRouter;
