2c543e310c7c7e2ad1d26c78ffaeefd1eb6ee928
Unchaptered <workstation19961002@gmail.com>
2022-09-02 21:36:52 +0900

    Update Auth Domain Dtos, 정규표현식 이나 에러 메세지 개선

--- src/models/dtos/user/signup.user.dto.ts
+++ src/models/dtos/user/signup.user.dto.ts
@@ -25,10 +25,29 @@ export class SignupUserDto implements IBaseDto, ISignupUserDto {
 
     getJoiInstance(): ObjectSchema<SignupUserDto> {
         return joi.object<SignupUserDto>({
-            email: joi.string().required().trim().max(20),
-            nickname: joi.string().required().trim().min(2).max(10),
-            password: joi.string().required().trim().min(8).max(15),
-            imageUrl: joi.string().required().max(255),
+            email: joi.string().required().trim().max(20).email().message("email 은 20자 이하여야 합니다."),
+            nickname: joi
+                .string()
+                .required()
+                .trim()
+                .regex(/[[ㄱ-ㅎㅏ-ㅣ]|[^\w\d]]/)
+                .min(2)
+                .max(10)
+                .message(
+                    "nickname 은 2자 이상 10자 이하의 한글, 영문, 숫자 조합입니다. (단모음, 단자음, 특수문자 제외)",
+                ),
+            password: joi
+                .string()
+                .required()
+                .trim()
+                .regex(/[!@#]{1,}/)
+                .regex(/[^[!@#]|[^\w\d]]|[ㄱ-ㅎㅏ-ㅣ가-힣]/)
+                .min(8)
+                .max(15)
+                .message(
+                    "password 는 8자 이상 15자 이하의 영문, 숫자 조합입니다. (특수문자 !@# 는 1개 가 반드시 포함되어야 합니다.)",
+                ),
+            imageUrl: joi.string().required().max(255).message("imageUrl 은 255 자 이하여야 합니다."),
         });
     }
 }
--- src/models/dtos/user/singin.user.dto.ts
+++ src/models/dtos/user/singin.user.dto.ts
@@ -19,8 +19,18 @@ export class SigninUserDto implements IBaseDto, ISigninUserDto {
 
     getJoiInstance(): ObjectSchema<SigninUserDto> {
         return joi.object<SigninUserDto>({
-            email: joi.string().required().trim().max(20),
-            password: joi.string().required().trim().min(8).max(15),
+            email: joi.string().required().trim().max(20).message("email 은 20자 이하여야 합니다."),
+            password: joi
+                .string()
+                .required()
+                .trim()
+                .regex(/[!@#]{1,}/)
+                .regex(/[^[!@#]|[^\w\d]]|[ㄱ-ㅎㅏ-ㅣ가-힣]/)
+                .min(8)
+                .max(15)
+                .message(
+                    "password 는 8자 이상 15자 이하의 영문, 숫자 조합입니다. (특수문자 !@# 는 1개 가 반드시 포함되어야 합니다.)",
+                ),
         });
     }
 }
