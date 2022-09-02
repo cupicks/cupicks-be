634c18f88d2a021e74818cc272a0b258378226d6
axis <axisotherwise@gmail.com>
2022-09-02 20:52:56 +0900

    Refactor  유저 인증 미들웨어 추가 버전 완료

--- src/routes/routers/auth.router.ts
+++ src/routes/routers/auth.router.ts
@@ -2,9 +2,10 @@ import { Router } from "express";
 import { preventLoginUserGuard } from "../../middlewares/guards/_.exporter";
 import { applicationXWwwFormUrlencodedFilter } from "../../middlewares/filters/_.exporter";
 import { AuthController } from "../controllers/_.exporter";
+import { MulterProvider } from "../../modules/_.loader";
 
 const authRouter: Router = Router();
-authRouter.post("/signup", preventLoginUserGuard, new AuthController().signup);
+authRouter.post("/signup", preventLoginUserGuard, MulterProvider.uploadSingle, new AuthController().signup);
 authRouter.post("/signin", applicationXWwwFormUrlencodedFilter, preventLoginUserGuard, new AuthController().signin);
 authRouter.get("/token", applicationXWwwFormUrlencodedFilter, new AuthController().publishToken);
 
