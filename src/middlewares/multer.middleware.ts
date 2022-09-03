96183dd2a96474e521e13356acddb0f9c7d28a0f
axis <axisotherwise@gmail.com>
2022-09-04 01:41:08 +0900

    Update multerMiddleware 유저 존재 로직 추가

--- src/middlewares/multer.middleware.ts
+++ src/middlewares/multer.middleware.ts
@@ -4,7 +4,18 @@ import { MulterProvider } from "../modules/_.loader";
 
 export const multerMiddleware: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
     MulterProvider.uploadSingle(req, res, (err) => {
-        if (err) return res.json("지원하는 이미지 형식을 확인해주세요.");
+        if (!res.locals)
+            return res.json({
+                isSuccess: false,
+                message: "존재하지 않는 유저입니다.",
+            });
+
+        if (err)
+            return res.json({
+                isSuccess: false,
+                message: "지원하는 이미지 형식을 확인해 주세요.",
+            });
+
         return next();
     });
 };
