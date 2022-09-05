4e7b62fa57f067862c864da7d4bbe54e966514cf
axis <axisotherwise@gmail.com>
2022-09-05 15:54:45 +0900

    Create comment 응답 타입 추가

--- src/constants/_.loader.ts
+++ src/constants/_.loader.ts
@@ -1,4 +1,5 @@
 import { TNODE_ENV } from "./types/t.node.env";
 import { TALGORITHM } from "./types/t.algorithm";
+import { IResponse, IResponseCustom } from "./types/t.comment.response";
 
-export { TNODE_ENV, TALGORITHM };
+export { TNODE_ENV, TALGORITHM, IResponse, IResponseCustom };
