bc439940e377b8a428d6440dae6a44a5bba7b15b
axis <axisotherwise@gmail.com>
2022-09-06 22:04:53 +0900

    Create IRecipeResponse 타입 생성

--- src/constants/_.loader.ts
+++ src/constants/_.loader.ts
@@ -1,5 +1,6 @@
 import { TNODE_ENV } from "./types/t.node.env";
 import { TALGORITHM } from "./types/t.algorithm";
 import { IResponse, IResponseCustom } from "./types/t.comment.response";
+import { IRecipeResponseCustom } from "./types/t.recipe.response";
 
-export { TNODE_ENV, TALGORITHM, IResponse, IResponseCustom };
+export { TNODE_ENV, TALGORITHM, IResponse, IResponseCustom, IRecipeResponseCustom };
