5839415b58656c083bda15d215d0a0c78a99da95
Unchaptered <workstation19961002@gmail.com>
2022-09-11 21:28:24 +0900

    Update Table Query for 사용자 이메일 발송횟수 제한, #116

--- sql/dev.sql
+++ sql/dev.sql
@@ -33,28 +33,36 @@ DROP TABLE IF EXISTS recipe;
 DROP TABLE IF EXISTS user_verify_list;
 
 CREATE TABLE IF NOT EXISTS user_verify_list (
-    user_verify_list_id     INT              NOT NULL    PRIMARY KEY AUTO_INCREMENT,
-    email                   VARCHAR(100)     NOT NULL    UNIQUE,
-    email_verified_date     DATETIME         NULL,
-    email_verified_token    VARCHAR(1000)    NULL,
-    email_verified_code     VARCHAR(6)       NULL,
-    is_verified_email       BOOLEAN          DEFAULT 0,
-    nickname                VARCHAR(20)      NULL,
-    nickname_verified_date  DATETIME         NULL,
-    nickname_verified_token VARCHAR(1000)    NULL,
-    is_verified_nickname    BOOLEAN          DEFAULT 0
+    user_verify_list_id         INT              NOT NULL    PRIMARY KEY AUTO_INCREMENT,
+    email                       VARCHAR(100)     NOT NULL    UNIQUE,
+    email_verified_date         DATETIME         NULL,
+    email_verified_token        VARCHAR(1000)    NULL,
+    email_verified_code         VARCHAR(6)       NULL,
+    is_verified_email           BOOLEAN          DEFAULT 0,
+    limited_email_count         INT              NULL, -- 1 일일 당 5 회만 가능
+    limited_email_overflow_date DATETIME         NULL, -- 오버 플로우 시, 1일 간 사용 불가
+    is_overflowed_email         BOOLEAN          DEFAULT 0,
+    nickname                    VARCHAR(20)      NULL,
+    nickname_verified_date      DATETIME         NULL,
+    nickname_verified_token     VARCHAR(1000)    NULL,
+    is_verified_nickname        BOOLEAN          DEFAULT 0
 );
 
 CREATE TABLE IF NOT EXISTS user (
-    user_id             INT             NOT NULL    PRIMARY KEY AUTO_INCREMENT,
-    user_verify_list_id INT             NOT NULL,
-    email               VARCHAR(100)     NOT NULL    UNIQUE,
-    nickname            VARCHAR(10)     NOT NULL    UNIQUE,
-    password            VARCHAR(255)    NOT NULL,
-    image_url           VARCHAR(255)    NULL,
-    refresh_token       VARCHAR(1000)   NULL,
-    created_at          DATETIME        NOT NULL    DEFAULT CURRENT_TIMESTAMP,
-    updated_at          DATETIME        NOT NULL    DEFAULT CURRENT_TIMESTAMP,
+    user_id                                 INT             NOT NULL    PRIMARY KEY AUTO_INCREMENT,
+    user_verify_list_id                     INT             NOT NULL,
+    email                                   VARCHAR(100)    NOT NULL    UNIQUE,
+    nickname                                VARCHAR(10)     NOT NULL    UNIQUE,
+    password                                VARCHAR(255)    NOT NULL,
+    image_url                               VARCHAR(255)    NULL,
+    refresh_token                           VARCHAR(1000)   NULL,
+    created_at                              DATETIME        NOT NULL    DEFAULT CURRENT_TIMESTAMP,
+    updated_at                              DATETIME        NOT NULL    DEFAULT CURRENT_TIMESTAMP,
+    reset_password_token                    VARCHAR(1000)   NULL,
+    reset_password_date                     DATETIME        NULL,
+    limited_send_passsword_count            INT             NULL, -- 1 일일 당 5회만 가능
+    limited_send_passsword_overflow_date    DATETIME        NOT NULL, -- 오버 플로우 시, 1일 간 사용 불가
+    is_overflowed_send_password             BOOLEAN         DEFAULT 0,
     FOREIGN KEY (user_verify_list_id) REFERENCES user_verify_list (user_verify_list_id)
         ON UPDATE CASCADE
         ON DELETE CASCADE
