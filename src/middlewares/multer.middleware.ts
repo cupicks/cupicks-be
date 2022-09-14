d459e239d79ef846cceddef885091450e30e2fb7
axis <axisotherwise@gmail.com>
2022-09-14 19:22:27 +0900

    Update  이슈를 반영한 MulterProvider 공급자 업데이트

--- src/middlewares/multer.middleware.ts
+++ src/middlewares/multer.middleware.ts
@@ -2,8 +2,26 @@ import { RequestHandler, Request, Response, NextFunction } from "express";
 
 import { MulterProvider } from "../modules/_.loader";
 
-export const multerMiddleware: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
-    MulterProvider.uploadSingle(req, res, (err) => {
+export const multerMiddlewareForProfile: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
+    MulterProvider.uploadImageProfile(req, res, (err) => {
+        if (!res.locals)
+            return res.status(401).json({
+                isSuccess: false,
+                message: "존재하지 않는 유저입니다.",
+            });
+
+        if (err)
+            return res.status(401).json({
+                isSuccess: false,
+                message: "지원하는 이미지 형식을 확인해 주세요.",
+            });
+
+        return next();
+    });
+};
+
+export const multerMiddlewareForComment: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
+    MulterProvider.uploadImageComment(req, res, (err) => {
         if (!res.locals)
             return res.status(401).json({
                 isSuccess: false,
