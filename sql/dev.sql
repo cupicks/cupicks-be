1a80559f0070c1bafb0d11d3b90b07e9e301b9b6
Unchaptered <workstation19961002@gmail.com>
2022-09-15 18:06:06 +0900

    Update SQL Table 리사이즈 이미지 저장 칼럼, #144

--- sql/dev.sql
+++ sql/dev.sql
@@ -56,6 +56,7 @@ CREATE TABLE IF NOT EXISTS user (
     nickname                                VARCHAR(10)     NOT NULL    UNIQUE,
     password                                VARCHAR(255)    NOT NULL,
     image_url                               VARCHAR(255)    NULL,
+    resized_url                             VARCHAR(255)    NULL,
     refresh_token                           VARCHAR(1000)   NULL,
     created_at                              DATETIME        NOT NULL    DEFAULT CURRENT_TIMESTAMP,
     updated_at                              DATETIME        NOT NULL    DEFAULT CURRENT_TIMESTAMP,
@@ -147,9 +148,10 @@ CREATE TABLE IF NOT EXISTS user_disfavor_recipe (
 -- comment 관련
 
 CREATE TABLE IF NOT EXISTS comment (
-    comment_id  INT                 NOT NULL    PRIMARY KEY AUTO_INCREMENT,
-    comment     VARCHAR(150)        NOT NULL,
-    image_url   VARCHAR(255)             NULL,
+    comment_id  INT             NOT NULL    PRIMARY KEY AUTO_INCREMENT,
+    comment     VARCHAR(150)    NOT NULL,
+    image_url   VARCHAR(255)    NULL,
+    resized_url VARCHAR(255)    NULL,
     created_at  DATETIME        NOT NULL    DEFAULT CURRENT_TIMESTAMP,
     updated_at  DATETIME        NOT NULL    DEFAULT CURRENT_TIMESTAMP
 );
