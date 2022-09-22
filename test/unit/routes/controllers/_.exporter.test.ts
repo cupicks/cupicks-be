0fc68821395404b33fc509dd74783d941866d34d
Unchaptered <workstation19961002@gmail.com>
2022-09-23 05:25:39 +0900

    Test Automated-Jest with esbuild-jest, #164

--- test/unit/routes/controllers/_.exporter.test.ts
+++ test/unit/routes/controllers/_.exporter.test.ts
@@ -1,3 +1,4 @@
 import * as exporter from "../../../../src/routes/controllers/_.exporter";
 
 it("controllerExporter has 4 Controller", () => expect(Object.keys(exporter).length).toBe(4));
+it("controllerExporter has 4 Controller", () => expect(Object.keys(exporter).length).toBe(4));
