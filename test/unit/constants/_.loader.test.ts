a56c42ab555e04faa2316ea407336e21d898ee35
Unchaptered <workstation19961002@gmail.com>
2022-09-29 22:16:59 +0900

    FixTest Unit Server Constants

--- test/unit/constants/_.loader.test.ts
+++ test/unit/constants/_.loader.test.ts
@@ -1,3 +1,3 @@
 import * as loader from "../../../src/constants/_.loader";
 
-it("constantsLoader has 5 Controller", () => expect(Object.keys(loader).length).toBe(6));
+it("constantsLoader has 6 Controller", () => expect(Object.keys(loader).length).toBe(6));
