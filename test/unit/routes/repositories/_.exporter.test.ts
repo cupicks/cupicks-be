e8ae366abef87773ea3392fe17fd3a3597d71bb8
Unchaptered <workstation19961002@gmail.com>
2022-10-26 10:25:21 +0900

    Test RepositoriesExporter, #274

--- test/unit/routes/repositories/_.exporter.test.ts
+++ test/unit/routes/repositories/_.exporter.test.ts
@@ -1,3 +1,3 @@
 import * as exporter from "../../../../src/routes/repositories/_.exporter";
 
-it("serviceExporter has 5 Controller", () => expect(Object.keys(exporter).length).toBe(8));
+it("serviceExporter has 5 Controller", () => expect(Object.keys(exporter).length).toBe(9));
