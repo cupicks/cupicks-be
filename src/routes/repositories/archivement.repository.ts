a0f9025e25b888b19e054ff7bb95bb57a0e15685
Unchaptered <workstation19961002@gmail.com>
2022-11-03 01:20:17 +0900

    Create ArchivementRepository, BedgeRepository

---
+++ src/routes/repositories/archivement.repository.ts
@@ -0,0 +1,54 @@
+import { EArchivementCode } from "../../models/enums/_.exporter";
+import { IArchivementPacket } from "../../models/_.loader";
+import { FieldPacket, PoolConnection, ResultSetHeader, RowDataPacket } from "mysql2/promise";
+
+export class ArchivementRepository {
+    public async isExistsActRecipeCount(conn: PoolConnection, userId: number) {
+        const isExistsQuery = `SELECT user_id FROM user_archivement_list WHERE user_id = ?;`;
+        const isExistsResult = await conn.query<RowDataPacket[][]>(isExistsQuery, [userId]);
+
+        const [rowDataPacket] = isExistsResult;
+        return rowDataPacket?.length === 1;
+    }
+
+    public async findActRecipeCount(conn: PoolConnection, userId: number): Promise<IArchivementPacket> {
+        //
+        const findQuery = `SELECT
+            user_id as userId,
+            archivement_name as archivementName,
+            archivement_count as archivementCount,
+            archivement_date as archivementDate
+        FROM user_archivement_list WHERE user_id = ?;`;
+        const findResult = await conn.query<IArchivementPacket[]>(findQuery, [userId]);
+
+        const [iArchivementPacketList] = findResult;
+        const [findedArchivementRow] = iArchivementPacketList;
+        return findedArchivementRow;
+    }
+
+    /**
+     * act_recipe_count
+     */
+    public async increaseActRecipeCount(
+        conn: PoolConnection,
+        userId: number,
+        target: EArchivementCode.레시피_작성_수,
+        datetime: string,
+    ) {
+        const updateQuery = `UPDATE user_archivement_list SET archivement_count = archivement_count + 1, archivement_date = ? WHERE user_id = ? AND archivement_name = ?;`;
+        const updateResult = await conn.query(updateQuery, [datetime, userId, target]);
+    }
+
+    /**
+     * act_recipe_count
+     */
+    public async createActRecipeCount(
+        conn: PoolConnection,
+        userId: number,
+        target: EArchivementCode.레시피_작성_수,
+        datetime: string,
+    ) {
+        const insertQuery = `INSERT INTO user_archivement_list (user_id, archivement_name, archivement_count, archivement_date) VALUES (?, ?, ?, ?);`;
+        const insertResult = await conn.query(insertQuery, [userId, target, 0, datetime]);
+    }
+}
---
+++ src/routes/repositories/bedge.repository.ts
@@ -0,0 +1,43 @@
+import { PoolConnection, ResultSetHeader, RowDataPacket } from "mysql2/promise";
+
+import { TBEDGE_CODE } from "../../constants/_.loader";
+import { EBedgeCode } from "../../models/enums/e.bedge.code";
+import { IBedgePacket } from "../../models/_.loader";
+
+export class BedgeRepsitory {
+    /**
+     * act_recipe_count
+     */
+    public increaeActRecipeCount(): void {
+        //
+    }
+
+    public async findSingleBedgeByUserId(
+        conn: PoolConnection,
+        userId: number,
+        bedgeCode: EBedgeCode,
+    ): Promise<IBedgePacket | null> {
+        const findQuery = `SELECT
+            user_id as userId,
+            bedge_name as bedgeName,
+            created_at as createdAt
+        FROM user_bedge_list WHERE user_id = ? AND bedge_name = ?;`;
+        const findResult = await conn.query<IBedgePacket[]>(findQuery, [userId, bedgeCode]);
+
+        const [findSetHeader] = findResult;
+
+        const bedge = findSetHeader[0];
+
+        return bedge ?? null;
+    }
+
+    public async publishBedge(
+        conn: PoolConnection,
+        userId: number,
+        bedgeCode: EBedgeCode,
+        datetime: string,
+    ): Promise<void> {
+        const insertQuery = `INSERT INTO user_bedge_list (user_id, bedge_name, created_at) VALUES (?, ?, ?);`;
+        await conn.query(insertQuery, [userId, bedgeCode, datetime]);
+    }
+}
