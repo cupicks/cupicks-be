56b46eea545bfd4d19b2efbab756c91a68b9126a
Unchaptered <workstation19961002@gmail.com>
2022-11-03 01:16:24 +0900

    Rename sql/migrations/yyyy-mm-dd MM-NN to MMNN

--- sql/migrations/2022-10-27 1028-create-user-bedge-list.sql
+++ sql/migrations/2022-10-27 1028-create-user-bedge-list.sql
@@ -6,13 +6,13 @@ DROP TABLE IF EXISTS user_bedge_list;
 
 CREATE TABLE IF NOT EXISTS user_bedge_list (
     user_id         INTEGER     NOT NULL,
-    badge_name      VARCHAR(10) NOT NULL,
+    bedge_name      VARCHAR(10) NOT NULL,
     created_at      DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES user (user_id)
         ON UPDATE CASCADE
         ON DELETE CASCADE,
-    FOREIGN KEY (badge_name) REFERENCES bedge (bedge_name)
+    FOREIGN KEY (bedge_name) REFERENCES bedge (bedge_name)
         ON UPDATE CASCADE
         ON DELETE CASCADE,
-    PRIMARY KEY (user_id, badge_name)
+    PRIMARY KEY (user_id, bedge_name)
 );
