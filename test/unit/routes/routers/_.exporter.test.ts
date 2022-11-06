a0e3e3b0ec2740ecac312f15e546c99949090810
unchaptered <workstation19961002@gmail.com>
2022-11-06 12:55:35 +0900

    Test exportert.length from 5 to 6 (router,controller,services)

--- test/unit/routes/routers/_.exporter.test.ts
+++ test/unit/routes/routers/_.exporter.test.ts
@@ -1,3 +1,3 @@
 import * as exporter from "../../../../src/routes/routers/_.exporter";
 
-it("routerExporter has 5 Controller", () => expect(Object.keys(exporter).length).toBe(5));
+it("routerExporter has 6 Controller", () => expect(Object.keys(exporter).length).toBe(6));
